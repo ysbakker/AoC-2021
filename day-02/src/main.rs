e5144296bd4cd39e9c646ca7a8923da80c7d9739
Yorrick Bakker <y.bakker@me.com>
2021-12-04 12:52:02 +0100

    Add day 2 part 2 solution

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -3,9 +3,13 @@ use get_input;
 fn main() {
     let input = get_input::get_input_as_strings("./input/day-02/input");
     let commands = parse_commands(&input);
-    println!("Part 1: {}", move_sub_and_get_product(&commands))
+    println!("Part 1: {}", move_sub_and_get_product(&commands));
+    println!("Part 2: {}", move_sub_and_get_product_with_aim(&commands));
 }
 
+/**
+ * returns (x_pos, depth)
+ */
 fn calculate_new_position(mut x_pos: i32, mut depth: i32, command: &(&str, u16)) -> (i32, i32) {
     match command.0 {
         "up" => depth -= command.1 as i32,
@@ -15,6 +19,28 @@ fn calculate_new_position(mut x_pos: i32, mut depth: i32, command: &(&str, u16))
     }
 
     return (x_pos, depth);
+}
+
+/**
+ * returns (x_pos, depth, aim)
+ */
+fn calculate_new_position_with_aim(
+    mut x_pos: i32,
+    mut depth: i32,
+    mut aim: i32,
+    command: &(&str, u16),
+) -> (i32, i32, i32) {
+    match command.0 {
+        "up" => aim -= command.1 as i32,
+        "down" => aim += command.1 as i32,
+        "forward" => {
+            x_pos += command.1 as i32;
+            depth += aim * command.1 as i32;
+        }
+        _ => (),
+    }
+
+    return (x_pos, depth, aim);
 }
 
 fn move_sub_and_get_product(commands: &Vec<(&str, u16)>) -> i32 {
@@ -29,6 +55,23 @@ fn move_sub_and_get_product(commands: &Vec<(&str, u16)>) -> i32 {
     return x_pos * depth;
 }
 
+fn move_sub_and_get_product_with_aim(commands: &Vec<(&str, u16)>) -> i32 {
+    let mut x_pos = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for command in commands {
+        let new_pos = calculate_new_position_with_aim(x_pos, depth, aim, command);
+        x_pos = new_pos.0;
+        depth = new_pos.1;
+        aim = new_pos.2;
+    }
+
+    return x_pos * depth;
+}
+
+/**
+ * returns Vec<(command, amount)>
+ */
 fn parse_commands(commands: &Vec<String>) -> Vec<(&str, u16)> {
     let mut parsed = Vec::<(&str, u16)>::new();
     for command in commands {
@@ -46,4 +89,11 @@ fn test_move_sub() {
     let input = get_input::get_input_as_strings("../input/day-02/test");
     let commands = parse_commands(&input);
     assert_eq!(150, move_sub_and_get_product(&commands))
+}
+
+#[test]
+fn test_move_sub_with_aim() {
+    let input = get_input::get_input_as_strings("../input/day-02/test");
+    let commands = parse_commands(&input);
+    assert_eq!(900, move_sub_and_get_product_with_aim(&commands))
 }
