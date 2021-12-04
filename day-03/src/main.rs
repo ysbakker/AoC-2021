d9a9532baa42c2b6475dd389d7256ae0ccf4dfb7
Yorrick Bakker <y.bakker@me.com>
2021-12-04 20:24:53 +0100

    Refactor gamma & epsilon rate calculation

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     )
 }
 
-fn find_gamma_rate(numbers: &[u32]) -> u32 {
+fn find_most_common_bits(numbers: &[u32]) -> u32 {
     let bits = get_max_bit_count(numbers);
     let mut compare = 1;
     let mut result = 0;
@@ -27,24 +27,18 @@ fn find_gamma_rate(numbers: &[u32]) -> u32 {
     result
 }
 
+fn find_gamma_rate(numbers: &[u32]) -> u32 {
+    find_most_common_bits(numbers)
+}
+
 fn find_epsilon_rate(numbers: &[u32]) -> u32 {
     let bits = get_max_bit_count(numbers);
-    let mut compare = 1;
-    let mut result = 0;
-    for i in 0..bits {
-        let mut amount_of_ones = 0;
-        for number in numbers {
-            if number & compare > 0 {
-                amount_of_ones += 1;
-            }
-        }
+    let most_common = find_most_common_bits(numbers);
+    most_common ^ (2u32.pow(bits) - 1)
+}
 
-        if amount_of_ones < numbers.len() / 2 {
-            result += 2u32.pow(i);
-        }
-        compare <<= 1
-    }
-    result
+fn find_oxygen_generator_rating(numbers: &[u32]) -> u32 {
+    0
 }
 
 /// Finds the bit count of the largest value in a collection of u32.
@@ -74,6 +68,11 @@ mod tests {
     }
     #[test]
     fn test_find_epsilon_rate() {
+        let input = get_input::get_input_from_binary_as_numbers(INPUT);
+        assert_eq!(9, find_epsilon_rate(&input));
+    }
+    #[test]
+    fn test_find_oxygen_generator_rating() {
         let input = get_input::get_input_from_binary_as_numbers(INPUT);
         assert_eq!(9, find_epsilon_rate(&input));
     }
